8197db0e69cf4bc0f6d8c5e6fde5296f535f34df
cap-diego <diegobuceta35@gmail.com>
2019-12-04 20:07:26 -0300

    adding args to test parameterized jobs

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 import unittest
+import argparse
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_isupper_always_true(self):
+        self.assertTrue('HOLA'.isupper())
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_isupper_always_false(self):
+        self.assertTrue('hola'.isupper())
 
 if __name__ == '__main__':
-    unittest.main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('job_id')
+    args = parser.parse_args()
+    print("Running case with job ID: {}".format(args.job_id))
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
